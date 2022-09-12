da2213de562283c85fef6ea23b853685302201e8
Jhener Daniel <jhener990@gmail.com>
2022-09-12 14:45:43 -0500

    pa borrar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
@@ -7,16 +7,23 @@ import Contact from './components/Contact'
 
 const App = () => {
   //portfolio whit routes and components
+  const [toglw, setToglw] = useState(false)
+
+  const handleClick = () => setToglw(!toglw)
   return (
     <div className='app'>
       <div className="bg"></div>
-      <h1>Hello World</h1>
+      <h1>Hello Abril</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
 
       </Routes>
+      <button onClick={handleClick}>dale click</button>
+      {
+        toglw && <h2>te encueras y me mandas pack ? saludos</h2>
+      }
     </div>
   )
 }
