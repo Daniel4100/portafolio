dc8c825593a369cafaa174ebd463b3a21b87c5f3
Jhener Daniel <jhener990@gmail.com>
2022-09-12 21:52:43 -0500

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,14 @@ const App = () => {
   return (
     <div className='app'>
       <div className="bg"></div>
-      <h1>Hello Abril</h1>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
 
       </Routes>
-      <button onClick={handleClick}>dale click</button>
-      {
-        toglw && <h2>te encueras y me mandas pack ? saludos</h2>
-      }
+      
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>que tal como estas ?</div>
-  )
-}
+    <ul className="icons-container">
+      <li className="icon"><Link to={'/about'} >
+        <i class="fa-solid fa-user"></i></Link>
+      </li>
+      <li className="icon"><a href="https://github.com/Daniel4100" target='_blanck'>
+        <i class="fa-brands fa-github"></i></a>
+      </li>
+      <li className="icon">
+        <i class="fa-solid fa-file"></i>
+      </li>
+      <li className="icon">
+        <i class="fa-solid fa-briefcase"></i>
+      </li>
+      <li className="icon">
+        <i class="fa-brands fa-linkedin"></i>
+      </li>
+    </ul>
+  );
+};
 
-export default Home
+export default Home;
