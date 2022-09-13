fe05782b90c89c254536accc1755b58c136f7d23
Jhener Daniel <jhener990@gmail.com>
2022-09-13 13:08:36 -0500

    about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about">
+      <div className="about-container">
+        <div className="content">
+          <h2 className="title">About me</h2>
+          <section className="about-text">
+            <p>
+              Hola mundo, me llamo Jhener Daniel Calvache Ceballos, soy
+              colombiano y soy un desarrollador web Front-end, me gusta la
+              programación, el diseño web y el café.
+            </p>
 
-export default About
+            <p>
+              Siempre es bueno aprender cosas nuevas y siempre es bueno ayudar a
+              los demás, y si es sobre programación mejor.
+            </p>
+
+            <p>
+              Soy fiel creyente de que la programación es una de las mejores
+              cosas que nos ha dado la humanidad, y que es una de las mejores
+              herramientas para poder cambiar el mundo.
+            </p>
+
+            <p>
+              Además me gusta cocinar, viajar, jugar videojuegos, ver series y
+              películas, y escuchar música con programación es lo mejor que hay.
+            </p>
+          </section>
+        </div>
+        <div className="content">
+          <h2 className="title">Contact</h2>
+          <div className="contact">
+            <a className="icon" href="mailto:ceballos990410@gmail.com">
+              <i className="fa-solid fa-envelope"></i>
+            </a>
+
+            <a
+              className="icon"
+              href="https://wa.me/573137448742"
+              target="_blanck"
+            >
+              <i className="fa-brands fa-whatsapp"></i>
+            </a>
+          </div>
+        </div>
+        <div className="about-img">
+          {/* <img src="https://i.ibb.co/7bQQYkX/IMG-20210410-134247.jpg" alt="Jhener Daniel" /> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,26 +3,33 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
-    <h2 className="name">Jhener Daniel</h2>
-    <p className="subtitle">Front-end  •  Web Developer</p>
-    <ul className="icons-container">
-      <li className="icon"><Link to={'/about'} >
-        <i class="fa-solid fa-user"></i></Link>
-      </li>
-      <li className="icon"><a href="https://github.com/Daniel4100" target='_blanck'>
-        <i class="fa-brands fa-github"></i></a>
-      </li>
-      <li className="icon">
-        <i class="fa-solid fa-file"></i>
-      </li>
-      <li className="icon">
-        <i class="fa-solid fa-briefcase"></i>
-      </li>
-      <li className="icon">
-        <i class="fa-brands fa-linkedin"></i>
-      </li>
-    </ul></>
+    <div className="home">
+      <h2 className="name">Jhener Daniel</h2>
+      <p className="subtitle">Front-end • Web Developer</p>
+      <ul className="icons-container">
+        <li className="icon">
+          <Link to={"/about"}>
+            <i className="fa-solid fa-user"></i>
+          </Link>
+        </li>
+        <li className="icon">
+          <a href="https://github.com/Daniel4100" target="_blanck">
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </li>
+        <li className="icon">
+          <i className="fa-solid fa-file"></i>
+        </li>
+        <li className="icon">
+          <i className="fa-solid fa-briefcase"></i>
+        </li>
+        <li className="icon">
+          <a href="https://www.linkedin.com/in/jhener-daniel/" target="_blanck">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
